12f81939e548611e6bf9f1279b7138064f10da24
kholilurrohman hendrata <hendrakid@gmail.com>
2022-12-07 02:47:20 +0000

    second commit

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
-const APP_ID = process.env['APP_ID']
-const GUILD_ID = process.env['GUILD_ID']
-const DISCORD_TOKEN = process.env['DISCORD_TOKEN']
-const PUBLIC_KEY = process.env['PUBLIC_KEY']
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildMembers,
-	],
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	client.commands.set(command.data.name, command);
+}
+// When the client is ready, run this code (only once)
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.once('ready', () => {
-    console.log('Ready!');
-  client.user.setPresence({ activities: [{ name: 'Discord' }], status: 'idle' });
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-
-client.login(DISCORD_TOKEN);
+client.login(token);
