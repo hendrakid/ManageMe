5e4a28da13b21803d7af182ba87ea19fce6e51da
kholilurrohman hendrata <hendrakid@gmail.com>
2022-12-07 05:58:42 +0000

    works

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+	],
+});
+
 
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
@@ -14,9 +22,10 @@ for (const file of commandFiles) {
 	const command = require(filePath);
 	client.commands.set(command.data.name, command);
 }
-// When the client is ready, run this code (only once)
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+
+
+client.once(Events.ClientReady, () => {
+	console.log('Ready!');
 });
 
 client.on(Events.InteractionCreate, async interaction => {
@@ -31,6 +40,35 @@ client.on(Events.InteractionCreate, async interaction => {
 	} catch (error) {
 		console.error(error);
 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	if (interaction.commandName === 'react-await') {
+		const message = await interaction.reply({ content: 'Awaiting emojis...', fetchReply: true });
+		message.react('👍').then(() => message.react('👎'));
+
+		const filter = (reaction, user) => {
+			return ['👍', '👎'].includes(reaction.emoji.name) && user.id === interaction.user.id;
+		};
+
+		message.awaitReactions({ filter, max: 1, time: 60000, errors: ['time'] })
+			.then(collected => {
+				const reaction = collected.first();
+
+				if (reaction.emoji.name === '👍') {
+					interaction.followUp('You reacted with a thumbs up.');
+				} else {
+					interaction.followUp('You reacted with a thumbs down.');
+				}
+			})
+			.catch(collected => {
+				console.log(`After a minute, only ${collected.size} out of 4 reacted.`);
+				interaction.followUp('You didn\'t react with neither a thumbs up, nor a thumbs down.');
+			});
 	}
 });
 
